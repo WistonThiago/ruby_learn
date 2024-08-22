2c8f3245ac80cf7788ac0d16f4fbc11b0c169d96
Wiston Thiago <wthyago-2000@hotmail.com>
2024-08-22 15:49:18 -0300

    Adding another solution for chall_two.

--- advanced/final_challenges/challenge_two.rb
+++ advanced/final_challenges/challenge_two.rb
@@ -14,3 +14,23 @@ end
 
 car = Car.new
 car.get_km
+
+# Other solution
+
+class Auto
+  def getting_km(phrase)
+    km = loc_km(phrase)
+    puts km
+  end
+
+  private
+
+  def loc_km(phrase)
+    /\d{2}km\/h/.match(phrase)
+  end
+end
+
+phrase = "A beetle traves at 80km/h"
+auto = Auto.new
+auto.getting_km(phrase)
+#it only shows the KM that matches with the phrase
